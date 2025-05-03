d3a8361f74250731d59d64d13e09d174891624e6
Jihchi Lee <achi@987.tw>
2025-05-03 13:39:20 +0300

    add more comments
    only print hello world

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 /// A "Hello, world!" program.
+///
+/// This is the best implementation of this program to ever exist.
 
 fn main() {
     println!("Hello, world!");
-    println!("Goodbye, world!");
 }
